e6535fbc6094df059a8ff51b898b7d37a070d988
wl30304 <47878923+wl30304@users.noreply.github.com>
2019-05-31 02:24:45 +0800

    Add files via upload

--- test/js/captcha.js
+++ test/js/captcha.js
@@ -7,7 +7,7 @@ function createCode(len)
             );               //創建需要的數據數組
             var idx,i;
             var result = '';   //返回的結果變量
-            for (i=0; i<4; i++) //根據指定的長度
+            for (i=0; i<len; i++) //根據指定的長度
             {   
                 idx = Math.floor(Math.random()*3); //獲得隨機數據的整數部分-獲取一個随機整数
                 result += seed[idx].substr(Math.floor(Math.random()*(seed[idx].length)), 1);//根據隨機樹獲取數據中的一個值
@@ -18,7 +18,7 @@ function createCode(len)
         function test() {
             var inputRandom=document.getElementById("inputRandom").value;
             var autoRandom=document.getElementById("autoRandom").innerHTML;
-            if(inputRandom==autoRandom||inputRandom!==null) {
+            if(inputRandom==autoRandom) {
                 alert("註冊成功，請重新登入");
                 window.location.replace("index.html");//跳轉到
             } else {
